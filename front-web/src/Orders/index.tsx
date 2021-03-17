384d810b010d7fd66fa6903eca7251ade6757b00
Paulo Honorato <paulohonoratos@hotmail.com>
2021-03-17 19:00:47 -0300

    Integration With DB

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import './styles.css'
 import StepHeader from './StepHeader';
+import ProductList from './ProductList';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepHeader />
+            <ProductList products={products} />
         </div>
     );
 }
