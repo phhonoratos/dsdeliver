65d3292169bbd5f7bb08ae46985972587ea47620
Paulo Honorato <paulohonoratos@hotmail.com>
2021-04-13 14:05:09 -0300

    changes

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -27,22 +27,22 @@ function OrderLocation({ onChangeLocation }: Props) {
         position: initialPosition
     });
           
-    const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
-        const response = await fetchLocalMapBox(inputValue);
+    // const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
+    //     const response = await fetchLocalMapBox(inputValue);
     
-        const places = response.data.features.map((item: any) => {
-            return ({
-                label: item.place_name,
-                value: item.place_name,
-                position: {
-                    lat: item.center[1],
-                    lng: item.center[0]
-                }
-            });
-        });
+    //     const places = response.data.features.map((item: any) => {
+    //         return ({
+    //             label: item.place_name,
+    //             value: item.place_name,
+    //             position: {
+    //                 lat: item.center[1],
+    //                 lng: item.center[0]
+    //             }
+    //         });
+    //     });
     
-        callback(places);
-    };
+    //     callback(places);
+    // };
     
     const handleChangeSelect = (place: Place) => {
         setAddress(place);
